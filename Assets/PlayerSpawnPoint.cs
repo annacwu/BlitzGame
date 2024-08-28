3b25d395c742dca9192c4c24a89f10df8531b70d
annacwu <annacwu04@gmail.com>
2024-08-27 23:36:22 -0300

    Add new spawn point script, get rid of old spawn points in scene

---
+++ Assets/PlayerSpawnPoint.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawnPoint : MonoBehaviour
+{
+    private void Awake() => PlayerSpawnSystem.AddSpawnPoint(transform);
+    private void OnDestroy() => PlayerSpawnSystem.RemoveSpawnPoint(transform);
+
+}
