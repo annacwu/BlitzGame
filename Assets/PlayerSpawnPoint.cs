2796ce7b2ba82e65e00b8f68b4907fb563fcd8ad
annacwu <annacwu04@gmail.com>
2024-08-30 15:21:31 -0300

    Draw spawn points with script, add as GameObjects

--- Assets/PlayerSpawnPoint.cs
+++ Assets/PlayerSpawnPoint.cs
@@ -7,4 +7,13 @@ public class PlayerSpawnPoint : MonoBehaviour
     private void Awake() => PlayerSpawnSystem.AddSpawnPoint(transform);
     private void OnDestroy() => PlayerSpawnSystem.RemoveSpawnPoint(transform);
 
+    // Gizmos are a cool thing that are basically the graphics so you can make them be drawn automatically upon spawn
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, 1f);
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + transform.right * 2);
+    }
+
 }
