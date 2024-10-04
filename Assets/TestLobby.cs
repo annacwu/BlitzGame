4ec887b82863dcef88d596bbfcc173200ae58798
annacwu <annacwu04@gmail.com>
2024-10-04 15:08:36 -0300

    Write script for most basic lobby features

--- Assets/TestLobby.cs
+++ Assets/TestLobby.cs
@@ -4,10 +4,15 @@ using Unity.Services.Authentication;
 using Unity.Services.Core;
 using Unity.Services.Lobbies;
 using Unity.Services.Lobbies.Models;
+using UnityEditor.Search;
 using UnityEngine;
 
 public class TestLobby : MonoBehaviour
 {
+
+    private Lobby hostLobby ;
+    private float heartbeatTimer;
+
     private async void Start() {
         await UnityServices.InitializeAsync();
 
@@ -19,15 +24,84 @@ public class TestLobby : MonoBehaviour
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
     }
 
+    private void Update() {
+        HandleLobbyHeartbeat();
+    }
+
+    // lobby dies if inactive for 30 sec, so this keeps it open
+    private async void HandleLobbyHeartbeat() {
+        if (hostLobby != null) {
+            heartbeatTimer -= Time.deltaTime;
+            // if timer has lapsed
+            if (heartbeatTimer < 0f) {
+                float heartbeatTimerMax = 15;
+                heartbeatTimer = heartbeatTimerMax;
+
+                await LobbyService.Instance.SendHeartbeatPingAsync(hostLobby.Id);
+            }
+        }
+    }
+
     private async void CreateLobby() {
         try {
             string lobbyName = "MyLobby";
             int maxPlayers = 4;
             Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers);
 
+            hostLobby = lobby;
             Debug.Log("Created Lobby! " + lobby.Name + " " + lobby.MaxPlayers);
+            PrintPlayers(hostLobby);
         } catch (LobbyServiceException e) {
             Debug.Log(e);
+        }
+    }
+
+    // find lobbies with unity's API
+    private async void ListLobbies() {
+        try {
+            // lists ALL lobbies active ever
+            QueryResponse queryResponse = await Lobbies.Instance.QueryLobbiesAsync();
+
+            Debug.Log("Lobbies found: " + queryResponse.Results.Count);
+            foreach (Lobby lobby in queryResponse.Results) {
+                Debug.Log(lobby.Name + " " + lobby.MaxPlayers);
+            }
+        } catch (LobbyServiceException e) {
+            Debug.Log(e);
+        }
+    }
+
+    // private async void JoinLobby() {
+    //     try {
+    //         QueryResponse queryResponse = await Lobbies.Instance.QueryLobbiesAsync();
+    //         // currently joins first one it finds
+    //         await Lobbies.Instance.JoinLobbyByIdAsync(queryResponse.Results[0].Id);
+    //     } catch (LobbyServiceException e) {
+    //         Debug.Log(e);
+    //     }
+    // }
+
+    private async void JoinLobbyByCode(string lobbyCode) {
+        try {
+            await Lobbies.Instance.JoinLobbyByCodeAsync(lobbyCode);
+            Debug.Log("Joined lobby by code " + lobbyCode);
+        } catch (LobbyServiceException e) {
+            Debug.Log(e);
+        }
+    }
+
+    private async void QuickJoinLobby() {
+        try {
+            await LobbyService.Instance.QuickJoinLobbyAsync();
+        } catch (LobbyServiceException e) {
+            Debug.Log(e);
+        }
+    }
+
+    private void PrintPlayers(Lobby lobby) {
+        Debug.Log("Players in Lobby " + lobby.Name);
+        foreach (Player player in lobby.Players) {
+            Debug.Log(player.Id);
         }
     }
 }
